4be3489ddea41f952c8f88f8175fd260955e0960
Azam <azam.turgunboyev@gmail.com>
2021-09-03 10:23:55 +0500

    API - Basket - Where to 'store' the basket - Adding Redis to the API - Creating the Basket repo & controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -27,6 +28,12 @@ namespace API
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_configuration.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+            
             services.AddApplicationServices();
             //services.AddSwaggerDocumentation();
             services.AddCors(opt =>
